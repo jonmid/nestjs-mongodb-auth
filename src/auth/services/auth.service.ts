de213082aed77cd320017fffb7dc2060b25a9477
JonMid <10jonmid@gmail.com>
2021-07-12 22:18:43 -0500

    [add] Config passport auth

---
+++ src/auth/services/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { UsersService } from './../../users/services/users.service';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async validateUser(email: string, password: string) {
+    const user = await this.usersService.findByEmail(email);
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (user && isMatch) {
+      const { password, ...rta } = user;
+      return rta;
+    }
+    return null;
+  }
+}
